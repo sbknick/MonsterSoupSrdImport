e8e36b11dd632caae752c86d27177d1700d7e063
Blair <b.knickerbocker@shieldsafety.co.uk>
2018-07-31 15:46:57 +0100

    refactored to use dynamicdata so expected results are tied more closely

--- MonsterSoupSrdImportTest/TraitMDParserTests.cs
+++ MonsterSoupSrdImportTest/TraitMDParserTests.cs
@@ -7,41 +7,9 @@ namespace MonsterSoupSrdImportTest
     [TestClass]
     public class TraitMDParserTests
     {
-
-        private static readonly Dictionary<string, string>[] ExpectedResults = new[]
+        [DataTestMethod, DynamicData(nameof(TemplateReplaces_TestCases), DynamicDataSourceType.Method)]
+        public void Should_ExtractReplacesFromTemplates(string template, string monsterTraitString, Dictionary<string, string> expectedResults)
         {
-            // idx 0
-            new Dictionary<string, string>
-            {
-                { "shortName", "smurf" }
-            },
-
-            // idx 1
-            new Dictionary<string, string>
-            {
-                { "shortName", "the aboleth" },
-                { "diceRoll", "1d4" },
-                { "savingThrow", "DC 14 Constitution saving throw" },
-            }
-        };
-
-        [DataRow("Hi, {shortName}!", "Hi, smurf!", 0, DisplayName = "Simple Smurf Test")]
-        [DataRow("While underwater, {shortName} is surrounded by transformative mucus. " +
-                 "A creature that touches {shortName} or that hits it with a melee attack while " +
-                 "within 5 feet of it must make a {savingThrow}. On a failure, " +
-                 "the creature is diseased for {diceRoll} hours. The diseased creature can breathe only " +
-                 "underwater.",
-                 "While underwater, the aboleth is surrounded by transformative mucus. " +
-                 "A creature that touches the aboleth or that hits it with a melee attack while " +
-                 "within 5 feet of it must make a DC 14 Constitution saving throw. On a failure, " +
-                 "the creature is diseased for 1d4 hours. The diseased creature can breathe only " +
-                 "underwater.",
-            1, DisplayName = "Aboleth, Mucous Cloud Test")]
-        [DataTestMethod]
-        public void Should_ExtractReplacesFromTemplates(string template, string monsterTraitString, int expectedResultIndex)
-        {
-            var expectedResults = ExpectedResults[expectedResultIndex];
-
             var extractor = new ReplaceExtractor();
 
             var replaces = extractor.GetReplacesFromTemplate(template, monsterTraitString);
@@ -52,6 +20,39 @@ namespace MonsterSoupSrdImportTest
             {
                 Assert.AreEqual(expectedResults[traitName], replaces[traitName]);
             }
+        }
+
+        public static IEnumerable<object[]> TemplateReplaces_TestCases()
+        {
+            yield return new object[]
+            {
+                "Hi, {shortName}!",
+                "Hi, smurf!",
+                new Dictionary<string, string>
+                {
+                    { "shortName", "smurf" }
+                },
+            };
+            
+            yield return new object[]
+            {
+                "While underwater, {shortName} is surrounded by transformative mucus. " +
+                "A creature that touches {shortName} or that hits it with a melee attack while " +
+                "within 5 feet of it must make a {savingThrow}. On a failure, " +
+                "the creature is diseased for {diceRoll} hours. The diseased creature can breathe only " +
+                "underwater.",
+                "While underwater, the aboleth is surrounded by transformative mucus. " +
+                "A creature that touches the aboleth or that hits it with a melee attack while " +
+                "within 5 feet of it must make a DC 14 Constitution saving throw. On a failure, " +
+                "the creature is diseased for 1d4 hours. The diseased creature can breathe only " +
+                "underwater.",
+                new Dictionary<string, string>
+                {
+                    { "shortName", "the aboleth" },
+                    { "diceRoll", "1d4" },
+                    { "savingThrow", "DC 14 Constitution saving throw" },
+                },
+            };
         }
     }
 }
